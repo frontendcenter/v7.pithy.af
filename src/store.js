cc0ff68fbdd42666ee793f5e9c963645aeb690bb
Glen Maddern <glenmaddern@gmail.com>
2017-09-01 16:42:45 +1000

    had to change the api slightly but quote page now uses it

--- src/store.js
+++ src/store.js
@@ -3,26 +3,35 @@ import { observable } from 'mobx'
 
 const store = observable({
   featured_quotes: null,
+  quotes: new Map(),
   error: false
 })
 
-const fetchAndGetTogetherAtLast = (endpoint, fetcher, getter) => {
-  const fetched = new Set()
+const fetchAndGetTogetherAtLast = (definition_function) => {
+  const getters = new Map()
 
   return (...args) => {
     const key = JSON.stringify(args)
-    if (!fetched.has(key)) {
-      fetched.add(key)
-      fetch(endpoint(...args))
+    if (!getters.has(key)) {
+      const { hit_endpoint, then_save, always_returning } = definition_function(...args)
+      getters.set(key, always_returning)
+
+      fetch(hit_endpoint)
         .then(response => response.json())
-        .then(fetcher, () => store.error = true)
+        .then(then_save, () => store.error = true)
     }
-    return getter()
+    return getters.get(key)()
   }
 }
 
-export const getFeaturedQuotes = fetchAndGetTogetherAtLast(
-  () => `${API}/quotes/featured`,
-  (quotes) => store.featured_quotes = quotes,
-  () => store.featured_quotes,
-)
+export const getFeaturedQuotes = fetchAndGetTogetherAtLast(() => ({
+  hit_endpoint: `${API}/quotes/featured`,
+  then_save: quotes => store.featured_quotes = quotes,
+  always_returning: () => store.featured_quotes,
+}))
+
+export const getQuoteById = fetchAndGetTogetherAtLast(id => ({
+  hit_endpoint: `${API}/quotes/${id}`,
+  then_save: quote => store.quotes.set(id, quote),
+  always_returning: () => store.quotes.get(id),
+}))
