06652bec96a245e6712e8c1b1b83370b073e4ad7
Glen Maddern <glenmaddern@gmail.com>
2017-09-02 12:01:09 +1000

    caching off the url works pretty well tbh

--- src/store.js
+++ src/store.js
@@ -8,41 +8,40 @@ const store = observable({
   error: false
 })
 
-const fetchAndGetTogetherAtLast = (definition_function) => {
-  const getters = new Map()
+const fetchAndGetTogetherAtLast = ({hit_endpoint, then_save, always_returning}) => {
+  const cache = new Map()
 
   return (...args) => {
-    const key = JSON.stringify(args)
-    console.log(definition_function('').hit_endpoint, JSON.stringify(args))
-    if (!getters.has(key)) {
+    const key = hit_endpoint(...args)
+    console.log(key)
+    if (!cache.has(key)) {
       console.log('💁')
-      const { hit_endpoint, then_save, always_returning } = definition_function(...args)
-      getters.set(key, always_returning)
+      cache.set(key, always_returning)
 
-      fetch(hit_endpoint)
+      fetch(key)
         .then(response => response.json())
-        .then(then_save, () => store.error = true)
+        .then(data => then_save(...args, data), () => store.error = true)
     } else {
       console.log('✋')
     }
-    return getters.get(key)()
+    return cache.get(key)(...args)
   }
 }
 
-export const getFeaturedQuotes = fetchAndGetTogetherAtLast(() => ({
-  hit_endpoint: `${API}/quotes/featured`,
-  then_save: quotes => store.featured_quotes = quotes,
+export const getFeaturedQuotes = fetchAndGetTogetherAtLast({
+  hit_endpoint: () => `${API}/quotes/featured`,
+  then_save: (quotes) => store.featured_quotes = quotes,
   always_returning: () => store.featured_quotes,
-}))
+})
 
-export const getQuoteById = fetchAndGetTogetherAtLast(id => ({
-  hit_endpoint: `${API}/quotes/${id}`,
-  then_save: quote => store.quotes.set(id, quote),
-  always_returning: () => store.quotes.get(id),
-}))
+export const getQuoteById = fetchAndGetTogetherAtLast({
+  hit_endpoint: id => `${API}/quotes/${id}`,
+  then_save: (id, quote) => store.quotes.set(id, quote),
+  always_returning: id => store.quotes.get(id),
+})
 
-export const getQuotesByAuthor = fetchAndGetTogetherAtLast(id => ({
-  hit_endpoint: `${API}/authors/${id}`,
-  then_save: quotes => store.quotes_by_author.set(id, quotes),
-  always_returning: () => store.quotes_by_author.get(id),
-}))
+export const getQuotesByAuthor = fetchAndGetTogetherAtLast({
+  hit_endpoint: id => `${API}/authors/${id}`,
+  then_save: (id, quotes) => store.quotes_by_author.set(id, quotes),
+  always_returning: id => store.quotes_by_author.get(id),
+})
