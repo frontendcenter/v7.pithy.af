17a24bad2e6bda38f9d135e32ed89b5059343c1b
Glen Maddern <glenmaddern@gmail.com>
2017-09-03 13:41:25 +1000

    ok this is working but its clearly terrible

--- src/store.js
+++ src/store.js
@@ -4,6 +4,8 @@ import { observable, toJS, runInAction, extendObservable, action } from 'mobx'
 const store = observable({
   featured_quote_ids: null,
   quotes_by_id: new Map(),
+  quotes_ids_by_author: new Map(),
+  quotes_ids_by_work: new Map(),
   cache: new Map(),
   error: false
 })
@@ -40,21 +42,35 @@ export const getFeaturedQuotes = fetchAndGetTogetherAtLast({
       return quote.id
     })
   },
-  always_returning: () => store.featured_quote_ids && store.featured_quote_ids.map(id => store.quotes_by_id.get(id))
+  always_returning: () => store.featured_quote_ids && store.featured_quote_ids.map(id => store.quotes_by_id.get(id)),
 })
 
 export const getQuoteById = fetchAndGetTogetherAtLast({
   hit_endpoint: id => `${API}/quotes/${id}`,
   then_save: (id, quote) => store.quotes_by_id.set(id, observable(quote)),
-  always_returning: id => store.quotes_by_id.get(id)
+  always_returning: id => store.quotes_by_id.get(id),
 })
 
 export const getQuotesByAuthor = fetchAndGetTogetherAtLast({
   hit_endpoint: id => `${API}/authors/${id}`,
+  then_save: (id,quotes) => {
+    store.quotes_ids_by_author.set(id, quotes.map(quote => {
+      store.quotes_by_id.set(quote.id, observable(quote))
+      return quote.id
+    }))
+  },
+  always_returning: id => store.quotes_ids_by_author.has(id) && store.quotes_ids_by_author.get(id).map(id => store.quotes_by_id.get(id)),
 })
 
 export const getQuotesByWork = fetchAndGetTogetherAtLast({
   hit_endpoint: id => `${API}/works/${id}`,
+  then_save: (id,quotes) => {
+    store.quotes_ids_by_work.set(id, quotes.map(quote => {
+      store.quotes_by_id.set(quote.id, observable(quote))
+      return quote.id
+    }))
+  },
+  always_returning: id => store.quotes_ids_by_work.has(id) && store.quotes_ids_by_work.get(id).map(id => store.quotes_by_id.get(id)),
 })
 
 export const upvoteQuote = action(id => {
