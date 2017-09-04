fa07332022091836e865b8726a2b33c7f8e973a4
Glen Maddern <glenmaddern@gmail.com>
2017-09-05 09:14:42 +1000

    ok storing just arrays of objects collapses this whole model

--- src/models/QuoteList.js
+++ src/models/QuoteList.js
@@ -1,36 +1,19 @@
-import { extendObservable } from 'mobx'
+import { extendObservable, observable } from 'mobx'
 
 import { API, fetch_json } from '../utils'
 import Quote from './Quote'
 
-class QuoteList {
-  constructor(endpoint) {
-    this.endpoint = endpoint
-    this.loading = false
-    extendObservable(this, {
-      quotes: null
-    })
+const lists_by_endpoint = observable.map()
+
+const QuoteList = {
+  for_endpoint: endpoint => {
+    if (!lists_by_endpoint.has(endpoint)) {
+      lists_by_endpoint.set(endpoint, null)
+      fetch_json(`${API}/${endpoint}`)
+        .then(data => lists_by_endpoint.set(endpoint, data.map(Quote.add)))
+    }
+    return lists_by_endpoint.get(endpoint)
   }
-
-  load() {
-    this.loading = true
-    fetch_json(`${API}/${this.endpoint}`)
-      .then(data => this.quotes = data.map(Quote.add))
-  }
-
-  to_json = () => {
-    if (!this.loading) this.load()
-    return this.quotes
-  }
-}
-
-const lists_by_endpoint = new Map()
-
-QuoteList.for_endpoint = endpoint => {
-  if (lists_by_endpoint.has(endpoint)) return lists_by_endpoint.get(endpoint).to_json()
-  const list = new QuoteList(endpoint)
-  lists_by_endpoint.set(endpoint, list)
-  return list.to_json()
 }
 
 export default QuoteList
