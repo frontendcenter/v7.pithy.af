f2ba30eb5680f6dcb112249257af27f3cebf1bc5
Glen Maddern <glenmaddern@gmail.com>
2017-09-05 10:13:45 +1000

    better names, but I dont love this inversion

--- src/models/QuoteList.js
+++ src/models/QuoteList.js
@@ -3,7 +3,7 @@ import { extendObservable, observable } from 'mobx'
 import { API, fetch_json } from '../utils'
 import Quote from './Quote'
 
-const LazyMap = (key_fn = String) => {
+const CachedMap = (key_fn = String) => {
   const _map = observable.map()
 
   return {
@@ -19,23 +19,14 @@ const LazyMap = (key_fn = String) => {
   }
 }
 
-const lists_by_endpoint = observable.map()
-const lazy_lists = LazyMap()
+const quote_lists = CachedMap()
 
 const QuoteList = {
-  _for_endpoint: endpoint => {
-    if (!lists_by_endpoint.has(endpoint)) {
-      lists_by_endpoint.set(endpoint, null)
+  for_endpoint: quote_lists.get_or_fetch(
+    endpoint =>
       fetch_json(`${API}/${endpoint}`)
-        .then(data => lists_by_endpoint.set(endpoint, data.map(Quote.add)))
-    }
-    return lists_by_endpoint.get(endpoint)
-  },
-  for_endpoint: lazy_lists.get_or_fetch(endpoint => {
-    console.log(endpoint)
-    return fetch_json(`${API}/${endpoint}`)
-      .then(data => data.map(Quote.add))
-  })
+        .then(data => data.map(Quote.add))
+  )
 }
 
 export default QuoteList
